4a163d15bf7c5d5e34375c6aea156a37a1520806
Victor Campos <victorcampos@ufrj.br>
2013-10-13 09:27:44 -0300

    adding select player

--- controllers/board_controller.py
+++ controllers/board_controller.py
@@ -3,14 +3,19 @@ from models.players.corner_player import CornerPlayer
 from views.console_board_view import ConsoleBoardView
 from models.board import Board
 
+import glob
+
 class BoardController:
   def __init__(self):
     self.board = Board(None)
     self.view  = ConsoleBoardView(self.board)
 
   def init_game(self):
-    self.white_player = CornerPlayer(Board.WHITE)
-    self.black_player = RandomPlayer(Board.BLACK)
+
+
+    self.white_player = self._select_player(Board.WHITE)
+    self.black_player = self._select_player(Board.BLACK)
+
     self.atual_player = self.black_player
 
     finish_game = 0
@@ -37,10 +42,10 @@ class BoardController:
     score = self.board.score()
     if score[0] > score[1]:
       print ""
-      print 'Jogador ' + Board.WHITE + ' Ganhou'
+      print 'Jogador ' + self.white_player.__class__.__name__ + '('+Board.WHITE+') Ganhou'
     elif score[0] < score[1]:
       print ""
-      print 'Jogador ' + Board.BLACK + ' Ganhou'
+      print 'Jogador ' + self.black_player.__class__.__name__ + '('+Board.BLACK+') Ganhou'
     else:
       print ""
       print 'Jogo terminou empatado'
@@ -50,3 +55,16 @@ class BoardController:
       return self.black_player
 
     return self.white_player
+
+  def _select_player(self, color):
+    players = glob.glob('./models/players/*_player.py')
+    print 'Selecione um dos players abaixo para ser o jogador '+color
+
+    for idx, player in enumerate(players):
+      print idx.__str__() + " - " + player
+
+    player = raw_input("Digite o numero do player que voce deseja: ")
+    module_globals = {}
+    execfile(players[int(player)], module_globals)
+    print module_globals.keys()
+    return module_globals[module_globals.keys()[len(module_globals.keys()) - 1]](color)
