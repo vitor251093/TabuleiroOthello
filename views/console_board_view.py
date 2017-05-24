e3ba42800664e34dd42c471edecae48bab2f2042
VitorMM <vitor251093@gmail.com>
2017-05-24 14:58:11 -0300

    Janela já é criada quando app inicia

--- views/console_board_view.py
+++ views/console_board_view.py
@@ -1,8 +1,23 @@
 """View do jogo de Othelo."""
-#import Tkinter as tk
+
+import Tkinter as Tkinter
 
 class ConsoleBoardView(object):
     """Objeto de view do jogo de Othelo."""
+
+    WINDOW_NAME = 'Othelo'
+    WINDOW_WIDTH = 600
+    WINDOW_HEIGHT = 600
+
+    master = Tkinter.Tk()
+    master.title(WINDOW_NAME)
+    master.minsize(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+    master.maxsize(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+
+    canvas = Tkinter.Canvas(master, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+    canvas.pack()
+
+    canvas.create_rectangle(30, 30, 10, 10, fill='#000')
 
     def __init__(self, board):
         self.board = board
@@ -10,4 +25,4 @@ class ConsoleBoardView(object):
     def update_view(self):
         """Atualiza a view do jogo."""
         print self.board
-        
+ 
